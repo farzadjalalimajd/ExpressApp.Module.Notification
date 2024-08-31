d37bb96799c78e25c60b79d9575744c2d41a9fdc
FarzadJM <farzadjalalimajd@gmail.com>
2024-08-31 13:37:11 +0330

    Refactoring

---
+++ Extensions/ServiceProviderExtension.cs
@@ -0,0 +1,15 @@
+﻿using ExpressApp.Module.Notification.Base;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExpressApp.Module.Notification.Extensions;
+
+public static class ServiceProviderExtension
+{
+    public static void InitializeNotificationScheduler(this IServiceProvider serviceProvider)
+    {
+        if (serviceProvider.GetRequiredService<INotificationScheduleAgent>() is INotificationScheduleAgent notificationScheduleWorker)
+        {
+            notificationScheduleWorker.Start();
+        }
+    }
+}
